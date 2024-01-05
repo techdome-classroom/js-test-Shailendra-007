79a5475a05eb437a9c6e1dfe935936a4b987f69d
Shailendra sahu <96808543+Shailendra-007@users.noreply.github.com>
2024-01-05 13:40:36 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,28 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const mapNum = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
+        let res = 0;
+        for (let i = 0; i < s.length; i++) {
+            const curr = mapNum[s[i]];
+            const val = mapNum[s[i + 1]];
     
+            if (val > curr) {
+                res += val - curr;
+                i++;
+            } else {
+                res += curr;
+            }
+        }
+        return res;
 };
 
 
